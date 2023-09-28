c008caa61936f4e240215f25f009f0f49e43808d
kelshardy <14024@coderacademy.edu.au>
2023-09-28 20:49:20 +1000

    feat: navigate hook implemented

--- src/App.js
+++ src/App.js
@@ -37,10 +37,10 @@ function App() {
       <BrowserRouter>
         <ul>
           <li>
-            <Link to="/1">Page 1</Link>
+            <Link to="/1/banana/muffin">Page 1</Link>
           </li>
           <li>
-            <Link to="/2">Page 2</Link>
+            <Link to="/2/3/4">Page 2</Link>
           </li>
         </ul>
 
