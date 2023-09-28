40182d1a8fc4d1263222c9867e92df1d02fe493b
kelshardy <14024@coderacademy.edu.au>
2023-09-28 20:35:23 +1000

    feat: params and hook example

---
+++ src/NumberGrabber.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+export default function NumberGrabber(){
+
+    const params = useParams();
+	// const {potato} = useParams();
+
+    return(
+        <div>
+            <h1>The number that this component grabbed is: {potato}</h1>
+        </div>
+    )
+}
